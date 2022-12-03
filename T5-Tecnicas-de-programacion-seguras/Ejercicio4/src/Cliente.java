b184da23f82c84bc87f5a6a201f49c436bf62392
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2022-12-03 13:03:25 +0100

    Ejercicio 4 ✅

--- T5-Tecnicas-de-programacion-seguras/Ejercicio4/src/Cliente.java
+++ T5-Tecnicas-de-programacion-seguras/Ejercicio4/src/Cliente.java
@@ -1,5 +1,12 @@
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.security.PublicKey;
+import java.util.Scanner;
 
 public class Cliente {
 
@@ -8,11 +15,74 @@ public class Cliente {
 
     public static void main(String[] args) {
         //Nos conectamos al servidor
+        Socket socket = null;
         try {
-            Socket socket = new Socket(HOST, PUERTO);
+            socket = new Socket(HOST, PUERTO);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         System.out.println("Conectado");
+
+        //Recibimos la clave publica del servidor
+        ObjectOutputStream output = null;
+        try {
+            output = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        ObjectInputStream input = null;
+        try {
+            input = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        PublicKey publicKey = null;
+        try {
+            publicKey = (PublicKey) input.readObject();
+        } catch (ClassNotFoundException | IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Clave publica recibida del servidor - clave publica: " + publicKey);
+
+        //Cifrar con la clave publica para enviar al servidor
+        Cipher cipher = null;
+        try {
+            cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        Scanner sc = new Scanner(System.in);
+        String mensajeEviar = "";
+        do{
+            System.out.println("Escribe texto para cifrar con clave publica del servidor: ");
+            mensajeEviar = sc.nextLine();
+            //Enviamos el mensaje cifrado al servidor
+            byte[] mensaje = null;
+            try {
+                mensaje = cipher.doFinal(mensajeEviar.getBytes());
+            } catch (IllegalBlockSizeException e) {
+                throw new RuntimeException(e);
+            } catch (BadPaddingException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                output.writeObject(mensaje);
+                output.flush();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } while (!mensajeEviar.equalsIgnoreCase("fin"));
+
+        System.out.println("Fin de la conexion con el servidor");
+
+        //Cerramos las conexiones
+        try {
+            output.close();
+            input.close();
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- T5-Tecnicas-de-programacion-seguras/Ejercicio4/src/Hilo.java
+++ T5-Tecnicas-de-programacion-seguras/Ejercicio4/src/Hilo.java
@@ -1,4 +1,12 @@
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.security.*;
 
 public class Hilo extends Thread {
 
@@ -9,6 +17,68 @@ public class Hilo extends Thread {
     }
 
     public void run() {
-        System.out.println("Hola");
+        //Lo primero, vamos a generar una clave publica y privada
+        //para ello, vamos a usar el algoritmo RSA
+        KeyPairGenerator keygen;
+        try {
+            keygen = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        KeyPair keys = keygen.generateKeyPair();
+        PrivateKey privateKey = keys.getPrivate();
+        PublicKey publicKey = keys.getPublic();
+
+        //Ahora, vamos a mandar la clave publica al cliente
+        ObjectOutputStream output = null;
+        try {
+            output = new ObjectOutputStream(c.getOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            output.writeObject(publicKey);
+            System.out.println("Clave publica enviada al cliente - clave publica: " + publicKey);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        ObjectInputStream input = null;
+        try {
+            input = new ObjectInputStream(c.getInputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Ahora, vamos a recibir el texto encriptado
+        Cipher descifrador = null;
+        try {
+            descifrador = Cipher.getInstance("RSA");
+            descifrador.init(Cipher.DECRYPT_MODE, privateKey);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+
+        byte[] mensajeEncriptado = null;
+        String mensajeDesencriptado = "";
+        do{
+            try {
+                mensajeEncriptado = (byte[]) input.readObject();
+                mensajeDesencriptado = new String(descifrador.doFinal(mensajeEncriptado));
+            } catch (IllegalBlockSizeException | BadPaddingException | ClassNotFoundException | IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Mensaje recibido: " + mensajeDesencriptado);
+        }while (!mensajeDesencriptado.equalsIgnoreCase("fin"));
+
+        //Cerrar los flujos
+        try {
+            input.close();
+            output.close();
+            c.close();
+            System.out.println("Fin de la conexion con el cliente");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
